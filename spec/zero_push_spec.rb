532964fc908cbccf872a0b71e3fe9f1c657549e9
Adam Duke <adam.v.duke@gmail.com>
2013-02-21 11:31:29 -0500

    namespace VCR cassette to the endpoint

--- spec/zero_push_spec.rb
+++ spec/zero_push_spec.rb
@@ -4,16 +4,19 @@ require 'spec_helper'
 
 describe ZeroPush do
   before do
-    VCR.insert_cassette "api"
     ZeroPush.url = "https://staging.zeropush.com"
     ZeroPush.auth_token = "acEJeDVC8if6XCxwe2js"
   end
 
-  after do
-    VCR.eject_cassette
-  end
-
   describe "/notify" do
+    before do
+      VCR.insert_cassette "notify"
+    end
+
+    after do
+      VCR.eject_cassette
+    end
+
     it "should construct the request" do
       response = ZeroPush.notify(device_tokens: ['abc'], alert: 'hi')
       response.status.must_equal 200
