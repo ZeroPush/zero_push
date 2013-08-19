02c1d67f0cf6c889260de8292588bac07156c4eb
Adam Duke <adam.v.duke@gmail.com>
2013-08-19 18:38:41 -0400

    added some tests for return types in the 1.x branch

--- spec/zero_push_spec.rb
+++ spec/zero_push_spec.rb
@@ -5,13 +5,23 @@ describe ZeroPush do
     ZeroPush.auth_token = ENV["AUTH_TOKEN"] || "test_token"
   end
 
-  describe "/verify_credentials" do
+  describe ".client" do
+    it "should return a Faraday client" do
+      ZeroPush.client.class.must_equal Faraday::Connection
+    end
+  end
+
+  describe ".verify_credentials" do
     before do
       VCR.insert_cassette "verify_credentials"
     end
 
     after do
       VCR.eject_cassette
+    end
+
+    it "should return a TrueClass" do
+      ZeroPush.verify_credentials.class.must_equal TrueClass
     end
 
     it "should verify credentials successfully" do
@@ -24,7 +34,7 @@ describe ZeroPush do
     end
   end
 
-  describe "/notify" do
+  describe ".notify" do
     before do
       VCR.insert_cassette "notify"
     end
@@ -33,13 +43,18 @@ describe ZeroPush do
       VCR.eject_cassette
     end
 
+    it "should return a Faraday::Response" do
+      response = ZeroPush.notify(device_tokens: ['abc'], alert: 'hi')
+      response.class.must_equal Faraday::Response
+    end
+
     it "should construct the request" do
       response = ZeroPush.notify(device_tokens: ['abc'], alert: 'hi')
       response.status.must_equal 200
     end
   end
 
-  describe "/register" do
+  describe ".register" do
     before do
       VCR.insert_cassette "register"
     end
@@ -48,13 +63,18 @@ describe ZeroPush do
       VCR.eject_cassette
     end
 
+    it "should return a Faraday::Response" do
+      response = ZeroPush.register('abc')
+      response.class.must_equal Faraday::Response
+    end
+
     it "should register the device" do
       response = ZeroPush.register('abc')
       response.status.must_equal 200
     end
   end
 
-  describe "/set_badge" do
+  describe ".set_badge" do
     before do
       VCR.insert_cassette "set_badge"
     end
@@ -63,19 +83,29 @@ describe ZeroPush do
       VCR.eject_cassette
     end
 
+    it "should return a Faraday::Response" do
+      response = ZeroPush.set_badge('abc', 10)
+      response.class.must_equal Faraday::Response
+    end
+
     it "should set the device's badge" do
       response = ZeroPush.set_badge('abc', 10)
       response.status.must_equal 200
     end
   end
 
-  describe "/inactive_tokens" do
+  describe ".inactive_tokens" do
     before do
       VCR.insert_cassette "inactive_tokens"
     end
 
     after do
       VCR.eject_cassette
+    end
+
+    it "should return a Faraday::Response" do
+      response = ZeroPush.inactive_tokens
+      response.class.must_equal Faraday::Response
     end
 
     it "should get a list of inactive tokens" do
