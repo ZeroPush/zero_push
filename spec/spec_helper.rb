d14b252871ad0aaf42ae2b34eecec18c44421b35
Adam Duke <adam.v.duke@gmail.com>
2014-01-03 09:41:24 -0500

    remove sick from vcr configuration [ci skip]

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,7 +8,9 @@ VCR.configure do |c|
   c.hook_into :faraday
   c.default_cassette_options = {
     record: :new_episodes,
-    match_requests_on: [:method, :uri, :headers],
-    serialize_with: :syck
+    match_requests_on: [:method, :uri, :headers]
   }
+  #Note: in some cases it may be necessary to add
+  #serialize_with: :syck to the default cassett options
+  #when recording new cassettes
 end
