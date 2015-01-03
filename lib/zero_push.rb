5fe34daca9a095a51f7a695f580b9e0f2d25bf0c
Stefan Natchev <stefan@natchev.com>
2015-01-03 17:13:13 -0500

    Update ZeroPush client to have a convenience method to handle multiple auth_tokens.
    This addresses issue #4

--- lib/zero_push.rb
+++ lib/zero_push.rb
@@ -4,46 +4,30 @@ require 'faraday'
 
 module ZeroPush
   class << self
-    attr_accessor :auth_token
+    extend Forwardable
+    attr_accessor :auth_token, :auth_tokens
 
-    def verify_credentials
-      client.verify_credentials
-    end
-
-    def notify(params)
-      client.notify(params)
-    end
-
-    def broadcast(params)
-      client.broadcast(params)
-    end
-
-    def subscribe(device_token, channel)
-      client.subscribe(device_token, channel)
-    end
-
-    def unsubscribe(device_token, channel)
-      client.unsubscribe(device_token, channel)
-    end
-
-    def register(device_token, channel=nil)
-      client.register(device_token, channel)
-    end
-
-    def unregister(device_token)
-      client.unregister(device_token)
-    end
-
-    def set_badge(device_token, badge)
-      client.set_badge(device_token, badge)
-    end
-
-    def inactive_tokens
-      client.inactive_tokens
-    end
+    def_delegators :client,
+      :verify_credentials,
+      :notify,
+      :broadcast,
+      :subscribe,
+      :unsubscribe,
+      :register,
+      :unregister,
+      :set_badge,
+      :inactive_tokens
 
     def client(auth_token = self.auth_token)
       ZeroPush::Client.new(auth_token)
+    end
+
+    def method_missing(method, *params, &block)
+      if auth_tokens.is_a?(Hash) && auth_tokens.keys.include?(method)
+        self.client(auth_tokens[method])
+      else
+        super
+      end
     end
   end
 end
--- spec/zero_push_spec.rb
+++ spec/zero_push_spec.rb
@@ -5,6 +5,17 @@ describe ZeroPush do
     ZeroPush.auth_token = ENV['AUTH_TOKEN'] || 'test_token'
   end
 
+  describe 'using different auth_tokens' do
+    it 'should accept a hash of tokens' do
+      ZeroPush.auth_tokens = {
+        apns: 'test-apns-token',
+        gcm: 'test-gcm-token'
+      }
+      ZeroPush.apns.auth_token.must_equal 'test-apns-token'
+      ZeroPush.gcm.auth_token.must_equal 'test-gcm-token'
+    end
+  end
+
   describe '.client' do
     it 'should return a client instance' do
       ZeroPush.client.class.must_equal ZeroPush::Client
